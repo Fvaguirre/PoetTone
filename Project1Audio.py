061b3ed907cfda2db9f078dd5eee6673d9574c16
Talmoon <elita@thetasoft.com>
2018-11-06 15:19:45 -0500

    updated audio
    more stuff

--- Project1Audio.py
+++ Project1Audio.py
@@ -2,30 +2,42 @@ import pygame;
 import win32com.client as wincl;
 from time import sleep; 
 
+globalPause = False;
 
-
-def playStanza(stanza, tone, cont): 
+def playStanza(stanza, tone, cont, end): 
     speak = wincl.Dispatch("SAPI.SpVoice");
     if (cont == False):
+        if (pygame.mixer_music.get_busy()):
+            pygame.mixer_music.fadeout(2000);
+            sleep(2);
         pygame.mixer_music.load(tone);
         pygame.mixer_music.set_volume(.5);
         pygame.mixer_music.play();
         sleep(3);
-    speak.Speak(stanza);
+    else: 
+        if (False == pygame.mixer_music.get_busy()):
+            pygame.mixer_music.rewind();
 
-    if (cont):
+    if (globalPause == False):
+        speakLine(stanza, speak);
+    else: 
+        pygame.mixer_music.pause();
+
+    if (end):
         pygame.mixer_music.fadeout(2000);
         sleep(2);
 
+def speakLine(line, speak):
+    speak.Speak(line);
 
-tones = {"Sad": "StillAlive.mp3"};
+tones = {"Sad": "StillAlive.mp3", "Anger": "StillAlive.mp3"};
 
 poem = [(\
 "Nature's first green is gold, \
-Her hardest hue to hold. \
+Her hardest hue to hold.", "Sad"),("\
 Her early leaf's a flower; \
 But only so an hour.", "Sad"),("\
-Then leaf subsides to leaf, \
+Then leaf subsides to leaf,", "Anger"),(" \
 So Eden sank to grief, \
 So dawn goes down to day, \
 Nothing gold can stay.", "Sad")];
@@ -35,13 +47,13 @@ def playPoem(poem): #expected that poem is an array in the form [(stanza, id), (
          stanza = poem[i][0];
          tone = poem[i][1];
          if (i == 0):
-             playStanza(stanza, tones[tone], False);
+             playStanza(stanza, tones[tone], False, False);
          elif (i == (len(poem)-1)):
-               playStanza(stanza, tones[tone], (poem[i-1][1] == poem[i][1]));
-         elif (poem[i][1] == poem[i+1][1]):
-             playStanza(stanza, tones[tone], True);
+               playStanza(stanza, tones[tone], (poem[i-1][1] == poem[i][1]), True);
+         elif (poem[i][1] == poem[i-1][1]):
+             playStanza(stanza, tones[tone], True, False);
          else:
-             playStanza(stanza, tones[tone], False);
+             playStanza(stanza, tones[tone], False, False);
 
 def run(poem):
     pygame.mixer.init();
